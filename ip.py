b97f55f7dc2847f4d9c7e6d3a048b7e62e2409dc
ideasorblogs <50910954+ideasorblogs@users.noreply.github.com>
2023-08-21 13:02:20 +0530

    Add files via upload

--- ip.py
+++ ip.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-ip = 'your ip address'  # replace with the IP address you want to look up
+ip = '37.19.221.200'  # replace with the IP address you want to look up
 
 response = requests.get(f'https://ipapi.co/{ip}/json/')
 result = response.json()
